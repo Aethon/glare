086dfce889a83a55bcbef44b2f98d6f1e5652c5e
Brent McCullough <brent.mccullough@code42.com>
2018-10-22 21:49:59 -0500

    shored up xmldoc comments and enabled generation

--- dotnet/GlareParser/Scanning/ScanTokenType.cs
+++ dotnet/GlareParser/Scanning/ScanTokenType.cs
@@ -1,10 +1,28 @@
 namespace Aethon.Glare.Scanning
 {
+    /// <summary>
+    /// Describes the type of a <see cref="ScanToken"/>
+    /// </summary>
     public enum ScanTokenType
     {
+        /// <summary>
+        /// An unbroken string of word characters.
+        /// </summary>
         Word,
+        
+        /// <summary>
+        /// A single non-word, non-space, non-newline character.
+        /// </summary>
         Mark,
+        
+        /// <summary>
+        /// An unbroken string of spaces, tabs and carriage returns (that are not followed by a line feed).
+        /// </summary>
         Space,
+        
+        /// <summary>
+        /// A newline: a single line-feed character or a carriage return and a line feed.
+        /// </summary>
         Newline
     }
 }
--- dotnet/GlareParser/Util/Preconditions.cs
+++ dotnet/GlareParser/Util/Preconditions.cs
@@ -2,8 +2,19 @@ using System;
 
 namespace Aethon.Glare.Util
 {
+    /// <summary>
+    /// Utilities for verifying preconditions in methods.
+    /// </summary>
     public static class Preconditions
     {
+        /// <summary>
+        /// Verifies a value is not null.
+        /// </summary>
+        /// <param name="value">Value to test.</param>
+        /// <param name="name">Name of the value source; typically the <code>nameof</code> the parameter being checked.</param>
+        /// <typeparam name="T">Value type.</typeparam>
+        /// <returns>The input value.</returns>
+        /// <exception cref="ArgumentNullException">The value was null.</exception>
         public static T NotNull<T>(T value, string name)
         {
             if (value == null)
@@ -11,6 +22,15 @@ namespace Aethon.Glare.Util
             return value;
         }
         
+        /// <summary>
+        /// Verifies that an array is not null and not empty.
+        /// </summary>
+        /// <param name="value">Array to test.</param>
+        /// <param name="name">Name of the array source; typically the <code>nameof</code> the parameter being checked.</param>
+        /// <typeparam name="T">Array element type.</typeparam>
+        /// <returns>The input value.</returns>
+        /// <exception cref="ArgumentNullException">The value was null.</exception>
+        /// <exception cref="ArgumentException">The array was empty.</exception>
         public static T[] NotNullOrEmpty<T>(T[] value, string name)
         {
             if (value == null)
@@ -20,6 +40,14 @@ namespace Aethon.Glare.Util
             return value;
         }
         
+        /// <summary>
+        /// Verifies that a string is not null and not empty.
+        /// </summary>
+        /// <param name="value">String to test.</param>
+        /// <param name="name">Name of the string source; typically the <code>nameof</code> the parameter being checked.</param>
+        /// <returns>The input value.</returns>
+        /// <exception cref="ArgumentNullException">The value was null.</exception>
+        /// <exception cref="ArgumentException">The string was empty.</exception>
         public static string NotNullOrEmpty(string value, string name)
         {
             if (value == null)
